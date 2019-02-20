b33a1490e437fb03cb4adbd9e6d4f8bbfeae6817
GeorgPandeh <georgpandeh@gmail.com>
2019-02-20 21:42:17 +0300

    delete console.log

--- src/index.js
+++ src/index.js
@@ -11,17 +11,14 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
     preferences[preferences[preferences[i]-1]-1] !==preferences[i] ){
 
       mass = preferences[preferences[preferences[i]-1]-1];
-      console.log('mass: '+mass)
-      console.log(preferences[i])
+
       if((mass-1)==i){
         count++;
         delete preferences[i];
         delete preferences[preferences[i]-1];
         delete preferences[preferences[preferences[i]-1]-1];
-      }
-      console.log('count: '+ count)
-      
-  }
+        }
+    }
   }
   return count;
 };
